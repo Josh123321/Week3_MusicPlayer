ecfacc4bdca1a136d5e78d6283a95fe394ada0c9
Josh Wu <josh.wu@tradevan.com.tw>
2019-08-05 09:29:59 +0800

    Updates

---
+++ precache-manifest.9d8d56a4fed9b2f0870c8aed94bb7299.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fc4b54a0a9c2c6df24c66bf0936bad",
+    "url": "/Week3_MusicPlayer/index.html"
+  },
+  {
+    "revision": "6b35062ebf437339240c",
+    "url": "/Week3_MusicPlayer/static/css/main.6530e348.chunk.css"
+  },
+  {
+    "revision": "4cc0b1a0d9d2ea2f8f8b",
+    "url": "/Week3_MusicPlayer/static/js/2.8acbbd01.chunk.js"
+  },
+  {
+    "revision": "6b35062ebf437339240c",
+    "url": "/Week3_MusicPlayer/static/js/main.06768bda.chunk.js"
+  },
+  {
+    "revision": "c9442656275a5d2f9577",
+    "url": "/Week3_MusicPlayer/static/js/runtime~main.1211d5e5.js"
+  },
+  {
+    "revision": "af5e4be14fd361d47f4e5ad5baac3663",
+    "url": "/Week3_MusicPlayer/static/media/cover_1.af5e4be1.jpg"
+  },
+  {
+    "revision": "5e1b485192a67fa7006b6b903b15eefc",
+    "url": "/Week3_MusicPlayer/static/media/cover_3.5e1b4851.jpg"
+  },
+  {
+    "revision": "9dd10d2800262f05282db7aa1d23e755",
+    "url": "/Week3_MusicPlayer/static/media/cover_4.9dd10d28.jpg"
+  },
+  {
+    "revision": "4d996cfa53e4a85208ad5a58173b6fc2",
+    "url": "/Week3_MusicPlayer/static/media/cover_5.4d996cfa.jpg"
+  },
+  {
+    "revision": "ff720dcc4514cba52387cc75b7361dc3",
+    "url": "/Week3_MusicPlayer/static/media/cover_6.ff720dcc.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Week3_MusicPlayer/static/media/logo.ee7cd8ed.svg"
+  }
+]);
